4e5e457b4e545fad2f470ffd76c99f0a506b23b5
BarbodH <habibi.k.barbod@gmail.com>
2022-01-07 15:19:04 -0500

    Linked questions/passages & added result display.

--- Resources/question-bank.js
+++ Resources/question-bank.js
@@ -1,9 +1,21 @@
-export const questions = [
+export const passages = [
   {
-    question: "What is 2 + 2?",
-    answers: [
-      { text: "4", correct: true },
-      { text: "22", correct: false },
+    passageText: "This is a sample text.",
+    questions: [
+      {
+        question: "What is 2 + 2?",
+        answers: [
+          { text: "4", correct: true },
+          { text: "22", correct: false },
+        ],
+      },
+      {
+        question: "What is 2 * 3?",
+        answers: [
+          { text: "6", correct: true },
+          { text: "23", correct: false },
+        ],
+      },
     ],
   },
 ];
--- Untimed Quiz/quiz-script.js
+++ Untimed Quiz/quiz-script.js
@@ -1,25 +1,57 @@
-import { questions } from "../Resources/question-bank.js";
+import { passages } from "../Resources/question-bank.js";
 
 const startButton = document.getElementById("start-btn");
 const nextButton = document.getElementById("next-btn");
 const questionContainerElement = document.getElementById("question-container");
+const passageElement = document.getElementById("passage");
 const questionElement = document.getElementById("question");
 const answerButtonsElement = document.getElementById("answer-buttons");
+const resultContainerElement = document.getElementById("result-container");
+const resultElement = document.getElementById("result");
 
-let shuffledQuestions, currentQuestionIndex;
+let shuffledPassages, shuffledQuestions, currentQuestionIndex, score;
+let correctAnswers = 0,
+  totalAnswers = 0;
 
-startButton.addEventListener("click", startGame);
+startButton.addEventListener("click", startQuiz);
 nextButton.addEventListener("click", () => {
   currentQuestionIndex++;
   setNextQuestion();
 });
 
-function startGame() {
-  startButton.classList.add("hide"); // classList
-  shuffledQuestions = questions.sort(() => Math.random() - 0.5); // shuffles questions
-  currentQuestionIndex = 0;
-  questionContainerElement.classList.remove("hide");
-  setNextQuestion();
+function startQuiz() {
+  startButton.classList.add("hide");
+  shuffledPassages = shuffleArray(passages); // shuffle passages in the passages
+  for (let i = 0; i < passages.length; i++) {
+    shuffledQuestions = shuffleArray(passages[i].questions); // shuffle questions of each passage
+    currentQuestionIndex = 0;
+    showPassage(passages[i]);
+    questionContainerElement.classList.remove("hide");
+  }
+}
+
+function showPassage(currentPassage) {
+  passageElement.innerText = currentPassage.passageText;
+  const button = document.createElement("button");
+  button.innerText = "Proceed to Questions";
+  button.classList.add("btn");
+  button.addEventListener("click", () => {
+    passageElement.classList.add("hide");
+    setNextQuestion();
+  });
+  answerButtonsElement.appendChild(button);
+}
+
+function showResult(score) {
+  questionContainerElement.classList.add("hide");
+  resultElement.innerText =
+    " " +
+    String(correctAnswers) +
+    " " +
+    String(totalAnswers) +
+    " " +
+    String(score);
+  resultContainerElement.classList.remove("hide");
 }
 
 function setNextQuestion() {
@@ -27,19 +59,17 @@ function setNextQuestion() {
   showQuestion(shuffledQuestions[currentQuestionIndex]);
 }
 
-function showQuestion(question) {
-  questionElement.innerText = question.question; // innerText?
-  question.answers.forEach((answer) => {
-    // ?
+function showQuestion(currentQuestion) {
+  questionElement.innerText = currentQuestion.question;
+  currentQuestion.answers.forEach((answer) => {
     const button = document.createElement("button");
     button.innerText = answer.text;
     button.classList.add("btn");
     if (answer.correct) {
-      // double check functionality
-      button.dataset.correct = answer.correct; // dataset
+      button.dataset.correct = answer.correct;
     }
     button.addEventListener("click", selectAnswer);
-    answerButtonsElement.appendChild(button); // appendChild
+    answerButtonsElement.appendChild(button);
   });
 }
 
@@ -54,6 +84,7 @@ function resetState() {
 function selectAnswer(e) {
   const selectedButton = e.target; // target
   const correct = selectedButton.dataset.correct;
+  updateScore(correct);
   setStatusClass(document.body, correct);
   Array.from(answerButtonsElement.children).forEach((button) => {
     setStatusClass(button, button.dataset.correct);
@@ -61,8 +92,9 @@ function selectAnswer(e) {
   if (shuffledQuestions.length > currentQuestionIndex + 1) {
     nextButton.classList.remove("hide");
   } else {
-    startButton.innerText = "Restart";
-    startButton.classList.remove("hide");
+    // display final result
+    score = (correctAnswers / totalAnswers) * 100;
+    showResult(score);
   }
 }
 
@@ -79,3 +111,21 @@ function clearStatusClass(element) {
   element.classList.remove("correct");
   element.classList.remove("wrong");
 }
+
+function updateScore(correct) {
+  totalAnswers++;
+  if (correct) {
+    correctAnswers++;
+  }
+}
+
+// Fisher-Yates algorithm - shuffle a given array
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+};
