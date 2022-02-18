bc4a6ccb12e0a1265fbfa61078c05fdd69bab385
BarbodH <habibi.k.barbod@gmail.com>
2022-02-18 15:32:57 -0500

    Added new passage; debugged container overflow

--- questionbanks/question-bank.js
+++ questionbanks/question-bank.js
@@ -105,7 +105,7 @@ export const passages = [
   }, {
     title: "",
     date: "March 2015, Section V Reading",
-    passageText: "Ice sculpting has been called a “spart”—part sport, part art. Like athletes, ice sculptors vie for medals at international competitions. Ice carving has been a Cultural Olympiad event at the Olympic and Paralympic Arts Festival since the 1988 Calgary Winter Games. The tools and the ice are heavy, so sculptors need endurance and strength. “It's physically demanding working non-stop under a time constraint,” says Dan Rebholtz, who has been carving for 22 years. He is a veteran of 100 competitions, a three-time world champion and a certified judge with the National Ice Carving Association. Most North American ice carvers have art training. Their tools include chainsaws, carving chisels, electrical sanders and torches, but most important, an imagination. “Our favourite word is ‘wow,'” says Rebholtz. It's the word most people say when they see sculptures such as <em>Surfacing Kingfisher</em> by the team of Junichi Nakamura (Japan) and Suguru Kanbayashi (Canada). Although ice sculptures are beautiful and their creation time-consuming, carvers must accept the potential for tragedy and the certainty of impermanence for their masterpieces. Melting can be a threat, but so too is crashing. Nakamura's team, which included Rebholtz, worked almost 20 hours a day for six days on <em>The Birth of the Bluebird</em>, a gigantic sculpture of a reclining woman, her arm reaching up to a bird. Just before the judging, it toppled, when its last supporting pillar was cut—a spectacle viewed over 10 000 times on YouTube. Sport? Art? With its challenge, skill, beauty and risk, ice carving is both",
+    passageText: "Ice sculpting has been called a “spart”—part sport, part art. Like athletes, ice sculptors vie for medals at international competitions. Ice carving has been a Cultural Olympiad event at the Olympic and Paralympic Arts Festival since the 1988 Calgary Winter Games. The tools and the ice are heavy, so sculptors need endurance and strength. “It's physically demanding working non-stop under a time constraint,” says Dan Rebholtz, who has been carving for 22 years. He is a veteran of 100 competitions, a three-time world champion and a certified judge with the National Ice Carving Association. Most North American ice carvers have art training. Their tools include chainsaws, carving chisels, electrical sanders and torches, but most important, an imagination. “Our favourite word is 'wow,'” says Rebholtz. It's the word most people say when they see sculptures such as <em>Surfacing Kingfisher</em> by the team of Junichi Nakamura (Japan) and Suguru Kanbayashi (Canada). Although ice sculptures are beautiful and their creation time-consuming, carvers must accept the potential for tragedy and the certainty of impermanence for their masterpieces. Melting can be a threat, but so too is crashing. Nakamura's team, which included Rebholtz, worked almost 20 hours a day for six days on <em>The Birth of the Bluebird</em>, a gigantic sculpture of a reclining woman, her arm reaching up to a bird. Just before the judging, it toppled, when its last supporting pillar was cut—a spectacle viewed over 10 000 times on YouTube. Sport? Art? With its challenge, skill, beauty and risk, ice carving is both",
     questions: [
       {
         question: "In what way is competitive ice sculpting different from an Olympic sporting event?",
@@ -161,6 +161,94 @@ export const passages = [
           { text: "phenomenon", correct: true }
         ]
       },
+    ]
+  }, {
+    title: "A Stunning Comeback to an Elite Sport",
+    date: "March 2014, Section IV Reading",
+    passageText: "The official line on Marie-Eve Chainey in the women's high jump read \“NH\”—shorthand for \“No Height\”—not exactly a fitting designation for an athlete who truly soared.\n For some athletes at the Canadian track and field championships in August 2010, a triumphant return meant posting fast times after a slow season. For Chainey, it meant returning to elite competition after a nine-year battle with kidney disease. Three years ago, she was unable to walk and even lacked the strength to wash her hair.\n As a 14-year-old, Chainey would often travel 820 kilometres from Kapuskasing to Toronto to train under coach Gary Lubin at York University. At 18, Chainey went to Spain to learn the language and continue her high-jump training. While there, she became so dizzy she had to be hospitalized. That's when she got the news: Her kidneys were no longer working. She hasn't known life without dialysis since.\n Healthy kidneys filter waste products from the blood. In dialysis treatment, a machine cleans the blood at regular intervals, for example, three times a week. Chainey has been using nocturnal dialysis, which works while she sleeps.\n Since her original diagnosis, Chainey has had to overcome four relapses and countless other obstacles, including going blind for two months. She was told over and over she'd never jump again because her muscles were too damaged. But for Chainey, jumping is like breathing.\n \“From when I got sick, the goal that I had was to just be back jumping,\” she said. \“Jumping was basically my happy place. Even now more so. Because I'm sick and there's so much going on, when I go to high jump, I don't think about anything else than just high jump and enjoying it. It is definitely my getaway. I feel normal because I don't have to think about anything else.\”\n"
+    + "So on the eve of the national championships, the 27-year-old was not about to be deterred by a difficult night of dialysis. \“I'm very stubborn, I'm very hard-headed ... I just had to find a way that I would be able to jump, no matter what.\”\n Chainey certainly felt jitters at the championships; her hands wouldn't stop shaking once the competition began. She didn't clear the starting height of 1.50 metres, which she had managed to get over in practice. Still, you'd be hard pressed to find a happier last-place finisher anywhere.\n \“Just being out there, especially when they lined us up and they introduced us to the crowd, it was a special moment that I'll always remember,\” she said. \“I didn't feel comfortable at first because I didn't feel I belonged. But although I didn't get a height, I still feel I belonged there. It felt awesome just to have the opportunity and experience this.\”\n Chainey says kidney disease has cured her of her perfectionism. \“I've always been a straight A student, always done well in sports and piano,\” she said. \“So when I got sick, my life wasn't perfect anymore. I had to learn how to live with what you have, that I had limits. That was a very good lesson for me, to know that things aren't always perfect but you can still make the best of it.\”\n Lubin is not surprised by her determination. “She used to come down from Kapuskasing, a 12-hour train ride ... in order to train. When I talk to my athletes about dedication, I say, ‘Don't tell me you came from Burlington. You think that's far? How about Kapuskasing?' This is the type of person she is.\”\n There are comebacks, and there are <em>comebacks</em>.",
+    questions: [
+      {
+        question: "Where was Chainey living when she was diagnosed with kidney disease?",
+        answers: [
+          { text: "Spain", correct: true },
+          { text: "Toronto", correct: false },
+          { text: "Burlington", correct: false },
+          { text: "Kapuskasing", correct: false }
+        ]
+      },
+      {
+        question: "Early on, how did Chainey demonstrate her dedication to high jumping?",
+        answers: [
+          { text: "She went to Kapuskasing to train competitively.", correct: false },
+          { text: "She competed at the Canadian track and field championships.", correct: false },
+          { text: "She competed even though she had received dialysis treatments.", correct: false },
+          { text: "She frequently travelled long distances to train at York University.", correct: true }
+        ]
+      },
+      {
+        question: "How is the information in paragraphs 1 to 3 organized?",
+        answers: [
+          { text: "present to past", correct: true },
+          { text: "cause and effect", correct: false },
+          { text: "similarities and differences", correct: false },
+          { text: "more important to less important", correct: false }
+        ]
+      },
+      {
+        question: "What is the purpose of the dashes in paragraph 1?",
+        answers: [
+          { text: "to set off an explanation", correct: true },
+          { text: "to replace quotation marks", correct: false },
+          { text: "to indicate a change in topic", correct: false },
+          { text: "to separate an idea and an example", correct: false }
+        ]
+      },
+      {
+        question: "Why is paragraph 5 important to the selection?",
+        answers: [
+          { text: "It reveals Chainey's determination.", correct: true },
+          { text: "It describes the dangers of dialysis.", correct: false },
+          { text: "It proves that Chainey should not compete.", correct: false },
+          { text: "It explains why high jumping is an elite sport.", correct: false }
+        ]
+      },
+      {
+        question: "Which word is closest in meaning to “deterred” as used in paragraph 7?",
+        answers: [
+          { text: "stopped", correct: true },
+          { text: "punished", correct: false },
+          { text: "confused", correct: false },
+          { text: "exhausted", correct: false }
+        ]
+      },
+      {
+        question: "What impact did kidney disease have on Chainey?",
+        answers: [
+          { text: "Chainey became dissatisfied with anything less than a win.", correct: false },
+          { text: "Chainey stopped using jumping as an escape from her troubles.", correct: false },
+          { text: "Chainey valued her fellow competitors as encouraging supporters.", correct: false },
+          { text: "Chainey no longer expected to be flawless in what she attempted.", correct: true }
+        ]
+      },
+      {
+        question: "Why was Chainey satisfied with her finish at the national championships?",
+        answers: [
+          { text: "She had jumped her personal best.", correct: false },
+          { text: "She had met her coach's expectations.", correct: false },
+          { text: "She had defeated her closest competitor.", correct: false },
+          { text: "She had returned to the sport she enjoyed.", correct: true }
+        ]
+      },
+      {
+        question: "What idea connects the introductory paragraph to the conclusion?",
+        answers: [
+          { text: "Chainey is a champion high jumper.", correct: false },
+          { text: "Chainey's last place finish is a victory.", correct: true },
+          { text: "Chainey's kidney disease is a curable illness.", correct: false },
+          { text: "Chainey has qualified for the national championships.", correct: false }
+        ]
+      }
     ]
   }
 ];
